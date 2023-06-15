859c18f71dbecf55e0eb42946a86cca9fcd81f63
Japio Jas <japiojas@live.nl>
2023-06-15 13:43:17 +0200

    Working Code A
    Yessssss

--- Algorithm.c
+++ Algorithm.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
+#include <stdlib.h>
 #include "headers.h"
+#include "comm.h"
 
 
 /*
@@ -138,11 +141,11 @@ void traceBack(int y, int x, int matrix[13][13], int n){
 }
 */
 
-int algorithm(int Dstations[], int t, int Stations[13][2]){
+int algorithm(int Dstations[], int t, int Stations[13][2], int startStation){
     int i = 0;
     int temp;
-    int startX = 2;
-    int startY = 8;
+    int startX = Stations[startStation][0];
+    int startY = Stations[startStation][1];
     int x = 0;
     int y = 0;
     float magnitude;
@@ -177,16 +180,16 @@ int algorithm(int Dstations[], int t, int Stations[13][2]){
 
     Dstations[currentI] = -1;
      for (i = 0; i < n; i++) {
-        printf("%d ", Dstations[i]);
+        //printf("%d ", Dstations[i]);
     }
     printf("\n");
-    return;
+    return currentStation;
 }
 
 int main(){
 
     int startX, startY, finishY, finishX, startStation, finishStation, steps;
-    int i = 1;
+    //int i = 1;
     int (*mazePointer)[13];
     int numDestinations = 3;
 
@@ -204,15 +207,351 @@ int main(){
      //x, y coords per station
     int stations[13][2] = {{0,0},
     {2,8}, {4,8}, {6,8}, {8,6}, {8,4}, {8,2}, {6,0}, {4,0}, {2,0}, {0,2}, {0,4}, {0,6}};
-    int destinationStations[3] = {3,-1,8};
+    int destinationStations[3] = {10,7,5};
     
-
-
-     algorithm(destinationStations, numDestinations, stations);
-
     //initialises the startstation
-    startStation = 12;
+    startStation = 1;
     char currentDirection = 'n';
+    char totalCommands_all[128] = "";
+
+    for(int i=0;i<3;i++){
+        int des = algorithm(destinationStations, numDestinations, stations, startStation);
+        
+        int desX = stations[des][0];
+        int desY = stations[des][1];
+        int beginX = stations[startStation][0];
+        int beginY = stations[startStation][1];
+
+        int nextX = desX - beginX;
+        int nextY = desY - beginY;
+
+        char commandsX[9] ="";
+        char commandsY[9] = "";
+        char totalCommands[18] = "";
+
+        char forward = 'f';
+        char left = 'l';
+        char right = 'r';
+
+        char tmp;
+
+        int done = 0;
+
+        if (startStation == 1 || startStation == 2 || startStation == 3 || startStation == 7 || startStation == 8 || startStation == 9) {
+            if(des == 4 || des == 5 || des == 6 || des == 10 || des == 11 || des == 12){
+                for (int i = 0; i<abs(nextY); i++) {
+                    strcat(commandsY, &forward);
+                }
+                
+                for (int j = 0; j<abs(nextX); j++) {
+                    while(done == 0){
+                    if((startStation == 7 || startStation == 8 || startStation == 9)){
+                        if (nextX > 0) {
+                            commandsX[0] = left;
+                            done = 1;
+                        }
+                        if(nextX < 0) {
+                            commandsX[0] = right;
+                            done = 1;
+                        }
+ 
+                    } else if ((startStation == 1 || startStation == 2 || startStation == 3)) {
+                        if (nextX < 0) {
+                            commandsX[0] = left;
+                            done = 1;
+                        }
+                        if(nextX > 0) {
+                            commandsX[0] = right;
+                            done = 1;
+                        }
+                    }
+                    }
+                    if(done != 0){
+                        strcat(commandsX, &forward);
+                    }
+                    
+                }
+
+                strcat(totalCommands, commandsY);
+                strcat(totalCommands, commandsX);
+
+            } else if(des == 1 || des == 2 || des == 3){
+                
+                done = 0;
+
+                if (startStation == 1 || startStation == 2 || startStation == 3){
+                    if(done == 0){                        
+                        if(nextX < 0) {
+                            commandsX[0] = left;
+                            tmp = left;
+                            done = 1;
+                        } else if(nextX > 0) {
+                            commandsX[0] = right;
+                            tmp = right;
+                            done = 1;
+                        }
+
+                        for (int i = 0; i+1<abs(nextX);i++) {
+                            strcat(commandsY, &forward);
+                        }
+                    }
+
+                    strcat(totalCommands, commandsX);
+                    strcat(totalCommands, commandsY);
+                    strcat(totalCommands, commandsX);
+                    
+                } else if(startStation == 7 || startStation == 8 || startStation == 9) {
+                    for (int i = 0; i<abs(nextY); i++){
+                        strcat(commandsY, &forward);
+                    }
+
+                    if(nextX < 0){
+                        commandsX[0] = right;
+                        for (int j = 0; j<abs(nextX); j++){
+                            strcat(commandsX,&forward);     
+                        }
+                        commandsX[abs(nextX)] = left;
+                    } else if (nextX > 0) {
+                        commandsX[0] = left;
+                        for (int j = 0; j+1<abs(nextX); j++){
+                            strcat(commandsX,&forward);     
+                        }
+                        commandsX[abs(nextX)] = right;
+                    } else {
+                        strcat(commandsX, &forward);
+                    }
+
+
+                    strcat(totalCommands, commandsY);
+                    strcat(totalCommands, commandsX);
+                }
+                
+
+            } else if(des == 7 || des == 8 || des == 9){
+                
+                done = 0;
+
+                if (startStation == 7 || startStation == 8 || startStation == 9){
+                    if(done == 0){                        
+                        if(nextX < 0) {
+                            commandsX[0] = right;
+                            tmp = right;
+                            done = 1;
+                        } else if(nextX > 0) {
+                            commandsX[0] = left;
+                            tmp = left;
+                            done = 1;
+                        }
+
+                        for (int i = 0; i+1<abs(nextX);i++) {
+                            strcat(commandsY, &forward);
+                        }
+                    }
+
+                    strcat(totalCommands, commandsX);
+                    strcat(totalCommands, commandsY);
+                    strcat(totalCommands, commandsX);
+                    
+                } else if(startStation == 1 || startStation == 2 || startStation == 3) {
+                    for (int i = 0; i<abs(nextY); i++){
+                        strcat(commandsY, &forward);
+                    }
+
+                    if(nextX < 0){
+                        commandsX[0] = left;
+                        for (int j = 0; j<abs(nextX); j++){
+                            strcat(commandsX,&forward);     
+                        }
+                        commandsX[abs(nextX)] = right;
+                    } else if (nextX > 0) {
+                        commandsX[0] = right;
+                        for (int j = 0; j+1<abs(nextX); j++){
+                            strcat(commandsX,&forward);     
+                        }
+                        commandsX[abs(nextX)] = left;
+                    } else {
+                        strcat(commandsX, &forward);
+                    }
+
+                    strcat(totalCommands, commandsY);
+                    strcat(totalCommands, commandsX);
+
+                }
+                
+
+            }
+
+            printf("\n\nTotal: %s\n\n", totalCommands);
+
+        } else if (startStation == 4 || startStation == 5 || startStation == 6 || startStation == 10 || startStation == 11 || startStation == 12) {
+            if(des == 1 || des == 2 || des == 3 || des == 7 || des == 8 || des == 9){
+                for (int i = 0; i<abs(nextX); i++) {
+                    strcat(commandsX, &forward);
+                }
+                
+                for (int j = 0; j<abs(nextY); j++) {
+                    while(done == 0){
+                    if((startStation == 10 || startStation == 11 || startStation == 12)){
+                        if (nextY > 0) {
+                            commandsY[0] = right;
+                            done = 1;
+                        }
+                        if(nextY < 0) {
+                            commandsY[0] = left;
+                            done = 1;
+                        }
+ 
+                    } else if ((startStation == 4 || startStation == 5 || startStation == 6)) {
+                        if (nextY < 0) {
+                            commandsY[0] = right;
+                            done = 1;
+                        }
+                        if(nextY > 0) {
+                            commandsY[0] = left;
+                            done = 1;
+                        }
+                    }
+                    }
+                    if(done != 0){
+                        strcat(commandsY, &forward);
+                    }
+                    
+                }
+
+                strcat(totalCommands, commandsX);
+                strcat(totalCommands, commandsY);
+
+            } else if(des == 4 || des == 5 || des == 6){
+                
+                done = 0;
+
+                if (startStation == 4 || startStation == 5 || startStation == 6){
+                    if(done == 0){                        
+                        if(nextY < 0) {
+                            commandsY[0] = right;
+                            tmp = right;
+                            done = 1;
+                        } else if(nextY > 0) {
+                            commandsY[0] = left;
+                            tmp = left;
+                            done = 1;
+                        }
+
+                        for (int i = 0; i+1<abs(nextY);i++) {
+                            strcat(commandsX, &forward);
+                        }
+                    }
+
+                    strcat(totalCommands, commandsY);
+                    strcat(totalCommands, commandsX);
+                    strcat(totalCommands, commandsY);
+                    
+                } else if(startStation == 10 || startStation == 11 || startStation == 12) {
+                    for (int i = 0; i<abs(nextX); i++){
+                        strcat(commandsX, &forward);
+                    }
+
+                    if(nextY < 0){
+                        commandsX[0] = left;
+                        for (int j = 0; j<abs(nextY); j++){
+                            strcat(commandsY,&forward);     
+                        }
+                        commandsY[abs(nextY)] = right;
+                    } else if (nextY > 0) {
+                        commandsY[0] = right;
+                        for (int j = 0; j+1<abs(nextY); j++){
+                            strcat(commandsY,&forward);     
+                        }
+                        commandsY[abs(nextY)] = left;
+                    } else {
+                        strcat(commandsY, &forward);
+                    }
+
+
+                    strcat(totalCommands, commandsX);
+                    strcat(totalCommands, commandsY);
+                }
+                
+
+            } else if(des == 10 || des == 11 || des == 12){
+                
+                done = 0;
+
+                if (startStation == 10 || startStation == 11 || startStation == 12){
+                    if(done == 0){                        
+                        if(nextY < 0) {
+                            commandsY[0] = left;
+                            tmp = left;
+                            done = 1;
+                        } else if(nextY > 0) {
+                            commandsY[0] = right;
+                            tmp = right;
+                            done = 1;
+                        }
+
+                        for (int i = 0; i+1<abs(nextY);i++) {
+                            strcat(commandsX, &forward);
+                        }
+                    }
+
+                    strcat(totalCommands, commandsY);
+                    strcat(totalCommands, commandsX);
+                    strcat(totalCommands, commandsY);
+                    
+                } else if(startStation == 4 || startStation == 5 || startStation == 6) {
+                    for (int i = 0; i<abs(nextX); i++){
+                        strcat(commandsX, &forward);
+                    }
+
+                    if(nextY < 0){
+                        commandsY[0] = right;
+                        for (int j = 0; j<abs(nextY); j++){
+                            strcat(commandsY,&forward);     
+                        }
+                        commandsY[abs(nextY)] = left;
+                    } else if (nextY > 0) {
+                        commandsY[0] = left;
+                        for (int j = 0; j+1<abs(nextY); j++){
+                            strcat(commandsY,&forward);     
+                        }
+                        commandsY[abs(nextY)] = right;
+                    } else {
+                        strcat(commandsY, &forward);
+                    }
+
+                    strcat(totalCommands, commandsX);
+                    strcat(totalCommands, commandsY);
+
+                }
+                
+
+            }
+
+            printf("\n\nTotal: %s\n\n", totalCommands);
+
+        }
+
+        strcat(totalCommands_all, totalCommands);
+
+        startStation = des;
+        
+    }
+
+    int a;
+    
+    char commands[] = {};
+    for(int i = 0; i<strlen(totalCommands_all); i++){
+        if (totalCommands_all[i] == 'f') {
+            commands[i] = 0x84;
+        } else if (totalCommands_all[i] == 'r') {
+            commands[i] = 0x81;
+        } else if (totalCommands_all[i] == 'l') {
+            commands[i] = 0x82;
+        }
+    }
+    int numCommands = sizeof(commands)/sizeof(char);       
+    a = runCar(numCommands, commands);
+    
 /*
     findNearestDestination(numDestinations, destinationChainHead, currentLocation, currentDirection);
     //target = findNearestDestination(destinationCoords);
